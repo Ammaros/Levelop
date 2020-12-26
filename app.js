32b97d2a2334d1a15c08b43f01c89f737d719cf9
Ammaros <ammarkashif00@gmail.com>
2020-12-25 19:39:53 -0500

    Changed Origin Again

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ mongoose.connect(process.env.MONGO_CONNECT, {useNewUrlParser: true, useFindAndMo
 
 // Middleware
 app.use(cors({
-    origin: "*"
+    origin: "https://dakheera47.github.io/"
 }));
 app.use(express.json());
 app.use(cookieParser());
@@ -27,7 +27,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(function(req, res, next) {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type');
-    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Origin", "https://dakheera47.github.io/");
     next();
 });
 
