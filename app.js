0e25a7eb1f7460e069f2189a7f1b061bf402d163
Ammaros <ammarkashif00@gmail.com>
2020-12-25 19:18:23 -0500

    Removed Credentials

--- app.js
+++ app.js
@@ -20,15 +20,13 @@ mongoose.connect(process.env.MONGO_CONNECT, {useNewUrlParser: true, useFindAndMo
 // Middleware
 app.use(cookieParser());
 app.use(cors({
-    origin: "*",
-    credentials: true,
+    origin: "*"
 }));
 app.use(express.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(function(req, res, next) {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
     res.setHeader("Access-Control-Allow-Origin", "*");
     next();
 });
